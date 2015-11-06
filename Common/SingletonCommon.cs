e8ab3571b0846b9e1f2f6327f3bc14c99b8410ab
xxblancxx <xxblancxx@hotmail.com>
2015-11-06 12:11:30 +0100

    Added LoginViewModel
    Added SingletonCommon
    Changed DataContext of MainPage
    Created Bindings to textboxes and Login button.
    
    Needs; Requester not done yet. No connection to API done yet, Login only gives Username and Password to Singleton Instance.
    Cannot test yet as i lack Hyper-V to run emulator. Will update now.

---
+++ Common/SingletonCommon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecast.it.Common
+{
+    class SingletonCommon
+    {
+        public static SingletonCommon SingletonInstance { get; private set; }
+        public string CurrentUsername { get; set; }
+        public string CurrentPassword { get; set; }
+        
+        //private constructor
+        private SingletonCommon(){}
+    }
+}
