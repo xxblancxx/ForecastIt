8d6761e70dd8bfb8add4b4410c72a045889fccca
NBMu <gc1994129@gmail.com>
2015-11-10 09:01:13 +0100

    test CreateTaskPage

--- View/ViewProjectDetails.xaml.cs
+++ View/ViewProjectDetails.xaml.cs
@@ -1,3 +1,4 @@
+<<<<<<< GU
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -96,3 +97,103 @@ namespace Forecast.it.View
 
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Forecast.it.ViewModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Forecast.it.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ViewProjectDetails : Page
+    {
+        public ViewProjectDetails()
+        {
+            this.InitializeComponent();
+
+            var projectdeatils= new ProjectViewModel();
+            var projectstage = projectdeatils.Project.projectStatus;
+            var projectowner = projectdeatils.Project.projectOwner;
+            var projectestimator = projectdeatils.Project.projectEstimator;
+            var projectmanager = projectdeatils.Project.projectManager;
+
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+    
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void OnFlyoutButtonClicked(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem item = sender as MenuFlyoutItem;
+
+            switch (item.Name)
+            {
+                case "userstory":
+                    this.Frame.Navigate(typeof (ChooseProjectPage));
+
+                    //
+                    break;
+
+                case "task":
+
+                    break;
+                case "project":
+                    //
+                    break;
+            }
+        }
+
+        private void OnFlyoutSetingButtonClicked(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem settingitem = sender as MenuFlyoutItem;
+
+            switch (settingitem.Name)
+            {
+                case "projectsetting":
+                    //
+                    break;
+
+                case "administration":
+                    break;
+                case "usersetting":
+                    //
+                    break;
+                case "support":
+                    break;
+                case "aboutforecast":
+                    break;
+                case "logout":
+                    break;
+
+
+            }
+        }
+
+    }
+}
+>>>>>>> local
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Forecast.it.Annotations;
+using Forecast.it.Common;
+
+namespace Forecast.it.ViewModel
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+        private SingletonCommon _singleton = SingletonCommon.SingletonInstance;
+        private RelayCommand _loginCommand;
+
+        public RelayCommand LoginCommand
+        {
+            get
+            {
+                _loginCommand = new RelayCommand(TryLoggingIn);
+                return _loginCommand;
+            }
+           private set { }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        private void TryLoggingIn()
+        {
+            _singleton.CurrentUsername = Username;
+            _singleton.CurrentPassword = Password;
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
