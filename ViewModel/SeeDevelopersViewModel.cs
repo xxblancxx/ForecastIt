8dce480e3c31dc345d9466dbf361ad6684a722db
tommy.frederiksen@gmail.com <Tommy.frederiksen@gmail.com>
2015-11-10 09:47:49 +0100

    VM update

--- ViewModel/SeeDevelopersViewModel.cs
+++ ViewModel/SeeDevelopersViewModel.cs
@@ -14,11 +14,13 @@ namespace Forecast.it.ViewModel
     public class SeeDevelopersViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<User> userCollection { get; set; }
+       
 
         public SeeDevelopersViewModel()
         {
             userCollection = new ObservableCollection<User>();
             const string url = "https://api.forecast.it/api/v1/users/";
+            userCollection.Add(new User("URL Test",1,"bob","bobsen", "BB","bob@email.farout",DateTime.Now, DateTime.Now, true,true,1,"bob knows his id","default startpage",4,""));
 
 
         }
