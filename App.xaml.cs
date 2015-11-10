e04e51a3d7d6f96024068a75d487df97f2e9a063
NBMu <gc1994129@gmail.com>
2015-11-10 08:41:36 +0100

    add TaskViewModel and connect data context wit view

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Forecast.it.View;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -105,7 +106,7 @@ namespace Forecast.it
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(CreateTaskPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
