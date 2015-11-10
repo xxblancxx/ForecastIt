114f9fcc371739c674e2dc5164b7363e8a190783
tommy.frederiksen@gmail.com <Tommy.frederiksen@gmail.com>
2015-11-10 10:24:34 +0100

    SeeDevelopersViewModel/model Done

--- ViewModel/SeeDevelopersViewModel.cs
+++ ViewModel/SeeDevelopersViewModel.cs
@@ -19,6 +19,7 @@ namespace Forecast.it.ViewModel
         {
             userCollection = new ObservableCollection<User>();
             const string url = "https://api.forecast.it/api/v1/users/";
+            userCollection.Add(new User("URL Test", 1, "bob", "bobsen", "BB", "bob@email.farout", DateTime.Now, DateTime.Now, true, true, 1, "bob knows his id", "default startpage", 4, ""));
 
 
         }
