b50fa91157ce8ac0a8a5d80f25a717f260854538
Dingi <janusringling2@gmail.com>
2015-11-24 08:46:27 +0100

    UserStoryVM

--- ViewModel/ViewUserStoryViewModel.cs
+++ ViewModel/ViewUserStoryViewModel.cs
@@ -14,20 +14,33 @@ namespace Forecast.it.ViewModel
 {
     public class ViewUserStoryViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<UserStory> userStoryCollection { get; set; }
+        public ObservableCollection<object> userStoryCollection { get; set; }
+        public ObservableCollection<UserStory> tempCollection { get; set; }
 
         Requester requester= new Requester();
 
         public ViewUserStoryViewModel()
         {
-            //GetUserStories();
+            GetUserStories(712);
         }
-        public ObservableCollection<UserStory> GetUserStories()
+        public ObservableCollection<object> GetUserStories(int projectId)
         {
-            var result = requester.GetRequestAsync<UserStory>(EndPoints.UserStories);
-            userStoryCollection = new ObservableCollection<UserStory>(result.Result);
+            var result = requester.GetRequestAsync<UserStory>(EndPoints.UserStories, projectId);
+            tempCollection = new ObservableCollection<UserStory>(result.Result);
+            IterateCollection();
             return userStoryCollection;
         }
+
+        public ObservableCollection<object> IterateCollection()
+        {
+            userStoryCollection = new ObservableCollection<object>();
+            foreach (var var in tempCollection)
+            {
+                userStoryCollection.Add(var.title);
+                userStoryCollection.Add(var.estimate);
+            }
+            return userStoryCollection;
+        } 
         
         public event PropertyChangedEventHandler PropertyChanged;
 
