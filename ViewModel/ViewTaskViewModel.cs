76f7dd36728133472befafc11d97b7720d5efdef
Dingi <janusringling2@gmail.com>
2015-11-26 08:57:01 +0100

    Awesomeshizle

--- ViewModel/ViewTaskViewModel.cs
+++ ViewModel/ViewTaskViewModel.cs
@@ -6,17 +6,46 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
 using Forecast.it.Annotations;
+using Forecast.it.Common;
 using Forecast.it.Model;
 using ForecastModel.Connection;
+using Task = Forecast.it.Model.Task;
 
 
 namespace Forecast.it.ViewModel
 {
     public class ViewTaskViewModel : INotifyPropertyChanged
     {
+        public static int TaskId { get; set; }
+
+        //public RelayArgCommand<int> SelectedTaskArgCommand          
+        //{
+        //    get { return _relayArgCommand ?? (_relayArgCommand = new RelayArgCommand<int>(OnSelectionChanged,OnSelectedTask)); }
+        //    set
+        //    {
+
+        //        _relayArgCommand = value;
+        //    }
+        //}
+
+        //private bool OnSelectedTask(int id)
+        //{
+            
+        //}
+
+        
+
+        public void OnSelectionChanged(int obj)
+        {
+            
+        }
         public ObservableCollection<Model.Task> TaskCollection { get; set; }
         Requester requester = new Requester();
+        private RelayArgCommand<int> _relayArgCommand;
+
 
         public ViewTaskViewModel()
         {
